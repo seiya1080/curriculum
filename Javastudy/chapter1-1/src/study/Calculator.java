6c182f0e544e5f31cdd254a17a8080cf1a801774
seiya1080 <57100710+seiya1080@users.noreply.github.com>
2021-04-12 23:56:43 +0900

    commit1-1

--- Javastudy/chapter1-1/src/study/Calculator.java
+++ Javastudy/chapter1-1/src/study/Calculator.java
@@ -2,15 +2,15 @@ package study;
 
 public class Calculator {
 
-    protected int plus1(int a) {
+    protected int plus(int a) {
         return a + 1;
     }
 
-    protected int plus2(int a, int b) {
+    protected int plus(int a, int b) {
         return a + b;
     }
 
-    protected int plus3(int a, int b, int c) {
+    protected int plus(int a, int b, int c) {
         return a + b + c;
     }
 
--- Javastudy/chapter1-1/src/study/Task.java
+++ Javastudy/chapter1-1/src/study/Task.java
@@ -31,11 +31,11 @@ public class Task extends Calculator {
      // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
      // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
 
-         int sumValue1 = super.plus1(10);
+         int sumValue1 = super.plus(10);
          System.out.println("plusメソッドの引数が一つの場合：" + sumValue1);
-         int sumValue2 = super.plus2(10, 20);
+         int sumValue2 = super.plus(10, 20);
          System.out.println("plusメソッドの引数が二つの場合：" + sumValue2);
-         int sumValue3 = super.plus3(10, 20, 30);
+         int sumValue3 = super.plus(10, 20, 30);
          System.out.println("plusメソッドの引数が三つの場合：" + sumValue3);
  }
 }
