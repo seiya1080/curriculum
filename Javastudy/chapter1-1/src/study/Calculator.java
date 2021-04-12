95780b4dcf94baa14222720a47e6cf1a704324d2
seiya1080 <57100710+seiya1080@users.noreply.github.com>
2021-04-12 23:55:20 +0900

    1-1

--- Javastudy/chapter1-1/src/study/Calculator.java
+++ Javastudy/chapter1-1/src/study/Calculator.java
@@ -2,15 +2,15 @@ package study;
 
 public class Calculator {
 
-    protected int plus(int a) {
+    protected int plus1(int a) {
         return a + 1;
     }
 
-    protected int plus(int a, int b) {
+    protected int plus2(int a, int b) {
         return a + b;
     }
 
-    protected int plus(int a, int b, int c) {
+    protected int plus3(int a, int b, int c) {
         return a + b + c;
     }
 
--- Javastudy/chapter1-1/src/study/Task.java
+++ Javastudy/chapter1-1/src/study/Task.java
@@ -1,21 +1,41 @@
 package study;
 
-// ① TaskクラスにCalculatorクラスを継承させなさい。
+//// ① TaskクラスにCalculatorクラスを継承させなさい。
+//public class Task extends Calculator {
+//    /**
+//     * タスクの実行
+//     */
+//        public void doTask() {
+//
+//        // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
+//        // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
+//
+//            Calculator sum = new Calculator();
+//            int sumValue1 = sum.plus(10);
+//            System.out.println("plusメソッドの引数が一つの場合：" + sumValue1);
+//            int sumValue2 = sum.plus(10, 20);
+//            System.out.println("plusメソッドの引数が二つの場合：" + sumValue2);
+//            int sumValue3 = sum.plus(10, 20, 30);
+//            System.out.println("plusメソッドの引数が三つの場合：" + sumValue3);
+//    }
+//}
+
+
+//① TaskクラスにCalculatorクラスを継承させなさい。
 public class Task extends Calculator {
-    /**
-     * タスクの実行
-     */
-        public void doTask() {
+ /**
+  * タスクの実行
+  */
+     public void doTask() {
 
-        // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
-        // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
+     // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
+     // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
 
-            Calculator sum = new Calculator();
-            int sumValue1 = sum.plus(10);
-            System.out.println("plusメソッドの引数が一つの場合：" + sumValue1);
-            int sumValue2 = sum.plus(10, 20);
-            System.out.println("plusメソッドの引数が二つの場合：" + sumValue2);
-            int sumValue3 = sum.plus(10, 20, 30);
-            System.out.println("plusメソッドの引数が三つの場合：" + sumValue3);
-    }
+         int sumValue1 = super.plus1(10);
+         System.out.println("plusメソッドの引数が一つの場合：" + sumValue1);
+         int sumValue2 = super.plus2(10, 20);
+         System.out.println("plusメソッドの引数が二つの場合：" + sumValue2);
+         int sumValue3 = super.plus3(10, 20, 30);
+         System.out.println("plusメソッドの引数が三つの場合：" + sumValue3);
+ }
 }
