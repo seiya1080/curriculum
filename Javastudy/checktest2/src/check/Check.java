2ad144e57adb19e8507bfd1eef20c65092298986
seiya1080 <57100710+seiya1080@users.noreply.github.com>
2021-04-29 22:51:38 +0900

    a

--- Javastudy/checktest2/src/check/Check.java
+++ Javastudy/checktest2/src/check/Check.java
@@ -1,22 +1,21 @@
 package check;
 
-public class Check {
+import constants.Constants;
+
+public class Check {    
     
-    private String firstname = "Namba";
-    private String lastname = "Seiya";
-    
-    private void printName() {
-    System.out.println("printNameメソッド→" + this.firstname + this.lastname);
-}
+     private static String firstname = "Namba";
+     private static String lastname = "Seiya";
+     private static String printName(String firstname,String lastname) {
+         return firstname + lastname;
+    }
+       
     public static void main(String[] args) {
-        Check chk = new Check();
-        chk.printName();
-        Pet pet = new Pet("java吉","hoge");
-        pet.introduce();
-        RobotPet Rp = new RobotPet("","ルーク");
-        Rp.introduce();
-        }
-}
-    
-    
-    
+    System.out.println("printNameメソッド→ " + printName(firstname,lastname));
+    Constants con =new Constants();
+    Pet pet = new Pet(con.CHECK_CLASS_JAVA,con.CHECK_CLASS_HOGE);
+    pet.introduce();
+    RobotPet Rp = new RobotPet(con.CHECK_CLASS_R2D2,con.CHECK_CLASS_LUKE);
+    Rp.introduce(); 
+ }
+}   
